d593c75a87d1e618db8b9cfa7ba300f2587dacbc
Kirill Klenov <horneds@gmail.com>
2014-06-20 04:06:07 +0400

    Update version

--- atmark.py
+++ atmark.py
@@ -23,7 +23,7 @@ LIST OF THE BUILT IN FUNCTIONS
 # Package information
 # ===================
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __project__ = "atmark"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
