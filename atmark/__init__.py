3e4ad7caa1ec0de0218fab06a21e35535e5d0438
Kirill Klenov <horneds@gmail.com>
2014-06-22 16:28:37 +0400

    Update version

--- atmark/__init__.py
+++ atmark/__init__.py
@@ -1,7 +1,7 @@
 # Package information
 # ===================
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 __project__ = "atmark"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
